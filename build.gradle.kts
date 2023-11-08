6cc1c1c19088561c8d8a5380dd43fac0f1b2abf3
agapelee824 <102851726+agapelee824@users.noreply.github.com>
2023-11-08 15:56:59 +0900

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.0.12"
+	id("io.spring.dependency-management") version "1.1.3"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
+}
+
+group = "com.kotlin"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.3.1")
+	runtimeOnly("com.mysql:mysql-connector-j")
+}
+
+allOpen {
+	annotation("jakarta.persistence.Entity")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
